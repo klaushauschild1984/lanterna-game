054e0cd63e920834689ccb319045cdbd92f84dcb
Klaus Hauschild <klaus.hauschild.1984@gmail.com>
2018-09-11 10:40:52 +0200

    implement reading ActionBinding from file

--- src/main/java/com/googlecode/lanterna/game/event/ActionBinding.java
+++ src/main/java/com/googlecode/lanterna/game/event/ActionBinding.java
@@ -19,9 +19,13 @@ package com.googlecode.lanterna.game.event;
 
 import com.googlecode.lanterna.input.KeyStroke;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Properties;
 
 /**
  * @author Klaus Hauschild
@@ -30,6 +34,45 @@ import java.util.Optional;
 public class ActionBinding {
 
     private final Map<KeyStroke, Action> actions = new HashMap<>();
+
+    public static ActionBinding from(final InputStream stream) {
+        try {
+            final Properties bindings = new Properties();
+            bindings.load(stream);
+
+            final ActionBinding actionBinding = new ActionBinding();
+            bindings.forEach((key, value) -> {
+                final KeyStroke keyStroke = getKeyStroke(key.toString());
+                final Action action = getAction(value.toString());
+                actionBinding.bind(keyStroke, action);
+            });
+            return actionBinding;
+        } catch (final IOException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+
+    private static KeyStroke getKeyStroke(final String key) {
+        return KeyStroke.fromString(key);
+    }
+
+    private static Action getAction(final String value) {
+        try {
+            final Class<?> actionClass = Class.forName(value);
+            return (Action) actionClass.newInstance();
+        } catch (final Exception exception) {
+            final int lastIndexOfDot = value.lastIndexOf(".");
+            final String className = value.substring(0, lastIndexOfDot);
+            final String fieldName = value.substring(lastIndexOfDot + 1);
+            try {
+                final Class<?> actionContainerClass = Class.forName(className);
+                final Field field = actionContainerClass.getField(fieldName);
+                return (Action) field.get(null);
+            } catch (final Exception exception2) {
+                throw new RuntimeException(exception2);
+            }
+        }
+    }
 
     public ActionBinding bind(final KeyStroke keyStroke, final Action action) {
         actions.put(keyStroke, action);
---
+++ src/test/java/com/googlecode/lanterna/game/event/ActionBindingTest.java
@@ -0,0 +1,41 @@
+/*
+ * This file is part of Lanterna Game.
+ *
+ * Lanterna Game is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Lanterna Game is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Lanterna Game.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.googlecode.lanterna.game.event;
+
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Klaus Hauschild
+ * @since 3.0.1
+ */
+public class ActionBindingTest {
+
+    @Test
+    public void fromTest() {
+        final ActionBinding actionBinding = ActionBinding.from(getClass().getResourceAsStream("binding.properties"));
+
+        assertThat(actionBinding.resolve(new KeyStroke(KeyType.F5)).get().getClass().getName(), is(ActionClass.class.getName()));
+        assertThat(actionBinding.resolve(new KeyStroke(KeyType.Escape)).get(), is(ActionEnum.EVENT));
+    }
+
+}
