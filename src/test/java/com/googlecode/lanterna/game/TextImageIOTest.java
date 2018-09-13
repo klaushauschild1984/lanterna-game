b8dc57d783b6c1c08aad3cf963da2ac0b03d7a6d
Klaus Hauschild <klaus.hauschild.1984@gmail.com>
2018-09-13 08:40:30 +0200

    move test image to its own directory add Gson as dependency

--- src/test/java/com/googlecode/lanterna/game/TextImageIOTest.java
+++ src/test/java/com/googlecode/lanterna/game/TextImageIOTest.java
@@ -1,31 +1,29 @@
 /*
  * This file is part of Lanterna Game.
  *
- * Lanterna Game is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Lanterna Game is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  *
- * Lanterna Game is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Lanterna Game is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with Lanterna Game.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with Lanterna Game. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 
 package com.googlecode.lanterna.game;
 
-import com.googlecode.lanterna.game.image.TextImageIO;
-import com.googlecode.lanterna.graphics.TextImage;
-import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 import java.io.File;
 import java.io.IOException;
 
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
+import org.junit.Test;
+
+import com.googlecode.lanterna.game.image.TextImageIO;
+import com.googlecode.lanterna.graphics.TextImage;
 
 /**
  * @author Klaus Hauschild
@@ -35,8 +33,8 @@ public class TextImageIOTest {
 
     @Test
     public void readTest() throws IOException {
-        final TextImage textImage = TextImageIO
-                .read(new File("src/test/resources/com/googlecode/lanterna/game/image"));
+        final TextImage textImage = TextImageIO.read(
+                        new File("src/test/resources/com/googlecode/lanterna/game/image/image"));
         assertThat(textImage.getSize().getColumns(), is(2));
         assertThat(textImage.getSize().getRows(), is(4));
     }
